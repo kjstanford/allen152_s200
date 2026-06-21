ef18527f6f21de6b0f1fe2bc15eed36e305db00f
Koustav Jana <kj2011@stanford.edu>
2026-06-21 01:01:31 -0700

    b1500A sampling

--- test_mylib.py
+++ test_mylib.py
@@ -7,6 +7,9 @@ smu_gate = b1500.smu3
 smu_drain = b1500.smu2
 smu_source = b1500.smu4
 
-meas_data = FET_sampling(b1500=b1500, smu_gate=smu_gate, smu_drain=smu_drain, smu_source=smu_source, gate_voltage=0.0, drain_voltage=0.0, source_voltage=0.0, hold_time=0.0, base_voltage=0.0, sampling_interval=1.0, nop=11, Irange='1 nA limited auto ranging', Vrange='2 V limited auto ranging')
+# meas_data = FET_sampling(b1500=b1500, smu_gate=smu_gate, smu_drain=smu_drain, smu_source=smu_source, gate_voltage=0.0, drain_voltage=0.0, source_voltage=0.0, hold_time=0.0, base_voltage=0.0, sampling_interval=0.1, nop=101, Irange='1 nA limited auto ranging', Vrange='2 V limited auto ranging')
 
-print(meas_data.head(10))
+# print(meas_data.head(10))
+
+FET_sampling_live(b1500=b1500, smu_gate=smu_gate, smu_drain=smu_drain, smu_source=smu_source, gate_voltage=0.0, drain_voltage=0.0, source_voltage=0.0, hold_time=0.0, base_voltage=0.0, sampling_interval=1.0, nop=11, Irange='1 nA limited auto ranging', Vrange='2 V limited auto ranging')
+
